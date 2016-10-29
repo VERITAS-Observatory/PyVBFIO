72340ed7ded1fe06728edabee68078323a373390
Tony Lin <xyxlin@gmail.com>
2016-10-28 22:57:22 -0400

    Change event loading mechanism

--- PyVBF.pyx
+++ PyVBF.pyx
@@ -1,4 +1,4 @@
-#distutils: language= c++
+#public public distutils: language= c++
 from libcpp.string cimport string 
 from libcpp cimport bool
 from cython cimport view
@@ -6,15 +6,21 @@ cimport numpy as np
 from VBF_C cimport *
 import numpy as np
 
+
+cdef struct Event:
+    VEvent* c_event
+    int     numSamples
+    int     numChannels
+    ubyte*  samplesPtr
+    
 cdef class PyVBFreader:
     cdef VBankFileReader* c_reader
     cdef VPacket*         c_packet
     cdef VSimulationHeader* c_simheader
     cdef VArrayEvent*       c_arrayevent
-    cdef VEvent*            c_event
     cdef bool             is_loaded
     cdef np.int8_t        packet_type
-    cdef view.array       samples 
+    cdef Event            c_evt_struct
    
     def __cinit__(self,str fname,bool map_index=True,bool read_only=True):
         self.c_reader = new VBankFileReader(fname,map_index,read_only)
@@ -54,20 +60,23 @@ cdef class PyVBFreader:
             self.c_arrayevent = self.c_packet.getArrayEvent() 
         self.is_loaded = True
 
-    cdef loadEvent(self,int i):
-        self.c_event = self.c_arrayevent.getEventByNodeNumber(i)  
-        
-    cdef loadSamples(self):
-         numSamples = self.c_event.getNumSamples()
-         numChannels = self.c_event.getNumChannels()
-         self.samples = <np.uint8_t[:numChannels, :numSamples]> self.c_event.getSamplePtr(0,0) 
+    cpdef loadEvent(self,int i):
+        if (self.c_arrayevent == NULL):
+           raise Exception("No ArrayEvent Loaded")
+        self.c_evt_struct.c_event = self.c_arrayevent.getEventByNodeNumber(i)  
+        if (self.c_evt_struct.c_event == NULL):
+            return 
+        else:
+            self.c_evt_struct.numSamples  = self.c_evt_struct.c_event.getNumSamples()
+            self.c_evt_struct.numChannels = self.c_evt_struct.c_event.getNumChannels()
+            self.c_evt_struct.samplesPtr = self.c_evt_struct.c_event.getSamplePtr(0,0)  
 
-    cpdef getSamples(self,int i):
-         self.loadEvent(i)
-         if (self.c_event == NULL):
-            return np.ones((500,20),dtype='uint8')*-1
-         self.loadSamples()
-         numpy_array = np.asarray(self.samples)
+    cpdef getSamples(self):
+         if (self.c_evt_struct.c_event == NULL):
+            return np.ones((500,20),dtype='uint8')
+         cdef int numChannels = self.c_evt_struct.numChannels  
+         cdef int numSamples = self.c_evt_struct.numSamples  
+         numpy_array = np.asarray(<np.uint8_t[:numChannels, :numSamples]>self.c_evt_struct.samplesPtr)
          return numpy_array 
  
     def get_sim_header(self):
